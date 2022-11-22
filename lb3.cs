68651e91c791b3556f34d57db6fac3a1a686e7f4
Hagfaner <118606538+Hagfaner@users.noreply.github.com>
2022-11-22 17:42:54 +0400

    Create lb3.cs

---
+++ lb3.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int h = 12;
+            int min = 30;
+            if (n == 0)
+            {
+                Console.WriteLine($"{h}:{min}");
+            }
+            else {
+                
+                min = 5 * (n+1);
+                if (min >= 60) { h++; min %= 60; }
+                h += n;
+                if (h >= 24) { h -= 24; }
+                Console.WriteLine($"{h}:{min}");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
